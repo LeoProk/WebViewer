a9dc140f2e8d7e35e120998df2b37f5c5c2d3f06
Leonid Prokopenko <just_beta@hotmail.com>
2017-11-15 03:55:16 +0200

    added permssion hanadling

--- app/src/main/java/leoprok/me/webviewer/MainActivity.java
+++ app/src/main/java/leoprok/me/webviewer/MainActivity.java
@@ -1,5 +1,10 @@
 package leoprok.me.webviewer;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -14,19 +19,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //create web view and get the factory with link and javascript enabled
-        final WebView webView = (WebView)findViewById(R.id.webView);
-        FactoryBuilder.getWebView(webView,this);
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        FactoryBuilder.getWebView(webView, this);
         //sets the title of actionbar
         setTitle(R.string.call);
     }
+
     //handles on menu item click lisnter
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch (item.getItemId()) {
+            //call when user click on the phone icon
+            case R.id.call:
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + R.string.phone));
+                //check if the permission is enabled
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    startActivity(intent);
+                }else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            16);
+                }
+
             default:
-            return super.onOptionsItemSelected(item)
-                    ;
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 16: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                }
+                return;
+            }
         }
     }
 }
